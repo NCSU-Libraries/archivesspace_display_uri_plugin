ab8d9f85496d9cc49b6174ff2c3c1e7047753398
trevorthornton <trthorn2@ncsu.edu>
2017-10-09 13:10:28 -0400

    first commit

---
+++ frontend/controllers/ajax_utility_controller.rb
@@ -0,0 +1,38 @@
+class AjaxUtilityController < ApplicationController
+
+  set_access_control  "view_repository" => [:get_json, :get_resource],
+                      "update_archival_record" => [:post]
+
+  def get_json
+    @uri = params[:uri]
+    @query_params = {}
+    params.each do |k,v|
+      if ![:controller, :action, :uri].include?(k)
+        if v.kind_of? Array
+          # Fix arrays (esp 'resolve') to work correctly
+          @query_params["#{k.to_s}[]"] = v
+        else
+          @query_params[k.to_s] = v
+        end
+      end
+    end
+    @response = JSONModel::HTTP::get_json("/#{@uri}",@query_params)
+    render json: @response, layout: false
+  end
+
+  def post_json
+    @uri = params[:uri]
+    @json = params[:json] || '{}'
+    JSONModel::HTTP::post_json(@uri, @json)
+    render json: @response, layout: false
+  end
+
+
+  def get_resource
+    if params[:resource_id]
+      @resource = JSONModel(:resource).find(params[:resource_id])
+      render json: JSON.generate(@resource)
+    end
+  end
+
+end
---
+++ frontend/routes.rb
@@ -0,0 +1,6 @@
+ArchivesSpace::Application.routes.draw do
+  # For API Utility
+  get '/api/resources/:resource_id' => 'ajax_utility#get_resource'
+  get '/api/*uri' => 'ajax_utility#get_json'
+  post '/api/*uri' => 'ajax_utility#post_json'
+end
